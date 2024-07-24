d9c89ba934ebd067c8e255050974043b99b580a1
Markus Kohlhase <markus.kohlhase@slowtec.de>
2024-07-24 15:40:14 +0200

    Update flake.nix to use nixrs crate

--- flake.nix
+++ flake.nix
@@ -1,10 +1,5 @@
-# please read flake introduction here:
-# https://fasterthanli.me/series/building-a-rust-service-with-nix/part-10#a-flake-with-a-dev-shell
 {
-  description = "The klick project flake";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
   outputs =
@@ -16,30 +11,19 @@
           pkgs = import nixpkgs {
             inherit system overlays;
           };
-          rust = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-          platform_packages =
-            if pkgs.stdenv.isLinux then
-              with pkgs; [ ]
-            else if pkgs.stdenv.isDarwin then
-              with pkgs.darwin.apple_sdk.frameworks; [
-                CoreFoundation
-                Security
-                SystemConfiguration
-              ]
-            else
-              throw "unsupported platform";
+          rust = pkgs.rust-bin.stable.latest.default;
         in
         with pkgs;
         rec {
           devShells.default = mkShell {
             buildInputs = [
-              pkgs.rust-bin.stable.latest.default
+              rust
               git
-              tig
               pkg-config
-              just                     # task runner
-              openssl                  # required for cargo-edit
-            ] ++ platform_packages;
+              openssl
+              nixVersions.latest
+              rustPlatform.bindgenHook
+            ];
           };
         }
       );
