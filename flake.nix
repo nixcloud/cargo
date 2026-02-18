773f514aca77f08102cd5216ced34a61cd0afee9
Joachim Schiele <js@lastlog.de>
2026-02-18 01:47:51 +0000

    Using nixpkgs 25.11

--- flake.nix
+++ flake.nix
@@ -1,12 +1,11 @@
 {
   description = "a flake to build libnix cargo";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     fenix.url   = "github:nix-community/fenix";
-    build-parser.url = "github:nixcloud/cargo-build_script_build-parser";
   };
   outputs =
-  { self, nixpkgs, flake-utils, fenix, build-parser } @ inputs:
+  { self, nixpkgs, flake-utils, fenix } @ inputs:
     flake-utils.lib.eachDefaultSystem
       (system:
         let
@@ -22,11 +21,11 @@
             if builtins.pathExists ./Cargo.dependencies.nix
               then import ./Cargo.dependencies.nix { inherit pkgs; }
               else { deps = {}; };
-          build_parser = pkgs.callPackage nix/build-rs-libnix/default.nix {
+          build_rs_libnix = pkgs.callPackage nix/build-rs-libnix/default.nix {
             inherit pkgs;
           };
           cargo-libnix = (import nix/derivations/default.nix {
-            inherit project_root pkgs external_crate_dependencies build_parser;
+            inherit project_root pkgs external_crate_dependencies build_rs_libnix;
             rustc = fenix.packages.${system}.stable.rustc;
             cargo = fenix.packages.${system}.stable.cargo;
            }).cargo-0_88_0-bin-25c525326f58ed30;
@@ -44,8 +43,7 @@
               # the toolchain used
               fenix.packages.${system}.stable.rustc
               #fenix.packages.${system}.stable.cargo
-              #cargo-libnix
-              #build_parser
+              cargo-libnix
               fenix.packages.${system}.stable.rust-src
               fenix.packages.${system}.stable.rustfmt
               fenix.packages.${system}.stable.clippy
